bafd43dbcc33c73f6f632745ecec9fae12f4d59f
dingZvel <zhezhe0214@126.com>
2022-05-09 08:52:51 +0800

    [fix] alpha enter nvim config file

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -13,20 +13,13 @@ dashboard.section.header.val = {
   [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 
-local EnterConfig
-if vim.fn.has "win32" == 1 or vim.fn.has "win64" == 1 then
-  EnterConfig = ":e %AppData%/../Local/nvim/init.lua <CR>"
-else
-  EnterConfig = ":e ~/.config/nvim/init.lua <CR>"
-end
-
 dashboard.section.buttons.val = {
   dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
   dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
   dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
   dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-  dashboard.button("c", "  Configuration", EnterConfig),
+  dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
   dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
