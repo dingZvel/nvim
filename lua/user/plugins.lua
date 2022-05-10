6f0f0bcf6b4dbb3a19a4d08b142a920fa95973b8
dingZvel <zhezhe0214@126.com>
2022-05-10 20:58:15 +0800

    [main] temporarily pin the commit of nvim-lsp-installer to validate current config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -109,7 +109,11 @@ return packer.startup(function(use)
 
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  -- TODO: migrate to new way of setting up LSP servers
+  use {
+    "williamboman/nvim-lsp-installer", -- simple to use language server installer
+    commit = "a1ed325024e5e8767b3ba26e66ae9c09743902a5",
+  }
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   -- use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
