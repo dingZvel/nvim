fe3124a85e4fd2041edd6707fc1dc0593c0fcfb0
dingZvel <zhezhe0214@126.com>
2022-05-15 20:41:11 +0800

    [feat] add tanvirtin/monokai.nvim colorscheme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -88,6 +88,7 @@ return packer.startup(function(use)
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
   -- use "tomasr/molokai"
+	-- use 'tanvirtin/monokai.nvim'
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
