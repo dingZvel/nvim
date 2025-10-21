7564b1b3e686b3d36cdc05e93072165dc5ed2a49
dingZvel <zhezhe0214@gmail.com>
2025-10-21 16:35:41 +0800

    [feat] use <Tab> to cycle the completion list and use <CR> to select and accept

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,9 +3,6 @@ return {
     "saghen/blink.cmp",
     opts = {
       completion = {
-        trigger = {
-          show_in_snippet = false,
-        },
         menu = {
           border = "rounded",
           winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
@@ -17,32 +14,22 @@ return {
         },
       },
       keymap = {
-        preset = "super-tab",
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<C-e>"] = { "hide", "fallback" },
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide', 'fallback' },
+        ['<CR>'] = { 'select_and_accept', 'fallback' },
 
-        ["<Tab>"] = {
-          function(cmp)
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
-          "snippet_forward",
-          "fallback",
-        },
-        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+        ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
 
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
-        ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
-        ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+        ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
 
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
 
-        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
       },
     },
   },
